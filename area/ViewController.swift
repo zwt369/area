0f0756d8cc8e4e4e5cf78e95e19447c613373051
mourui <mourui@mouruidemac-mini.local>
2016-12-01 10:49:22 +0800

    Initial Commit

---
+++ area/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  area
+//
+//  Created by mourui on 16/12/1.
+//  Copyright © 2016年 mourui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
